73af62845a9776f92309e60bf4069c5fbae7b5dc
matteo.zh <37191615+MatteoZhang@users.noreply.github.com>
2019-05-08 11:20:56 +0200

    Add files via upload

--- lab1/ex1.2.py
+++ lab1/ex1.2.py
@@ -4,6 +4,8 @@ from statistics import mean
 import random
 import copy
 
+
+
 data = open("names.txt", "r")
 G = nx.Graph()
 i = 0
@@ -28,18 +30,18 @@ print("tot nodes: ", n_nodes)
 list_of_active = []
 dictionary_of_active = {}
 
-INITIAL_SANE = 90  # choose from 0 to 100
+INITIAL_SANE = 9999900  # choose from 0 to 100
 NEIGHBOURS = 0.9  # choose from 0 to 1 if % of neighours are infected then also the node
+RESISTANCE = 50
 
 for i in list(G.nodes):
-    x = random.randint(0, 100)
+    x = random.randint(0, 10000000)
     if x < INITIAL_SANE:
         y = 0
     else:
         y = 1
     dictionary_of_active[i] = {'active': y}
 
-nx.set_node_attributes(G, 0, 'active')
 nx.set_node_attributes(G, dictionary_of_active)
 
 not_inf = 0
@@ -50,18 +52,19 @@ for i in list(G.nodes):
 
 surv = not_inf/n_nodes
 
+r = random.randint(0, 100)
 surv_list = []
 surv_list.append(surv)
 G_old = copy.deepcopy(G)
-for i in range(10):
+for i in range(20):
     for j in list(G.nodes):
         if G.nodes[j]['active'] == 0:
             infection_rate = 0
             for k in list(G_old.adj[j]):
                 if G_old.nodes[k]['active'] == 1:
                     infection_rate += 1
-            infetion_rate=infection_rate/len(list(G_old.adj[j]))
-            if infection_rate > NEIGHBOURS:
+            infetion_rate = infection_rate/len(list(G_old.adj[j]))
+            if infection_rate > NEIGHBOURS and RESISTANCE > r:
                 G.nodes[j]['active'] = 1
     G_old = copy.deepcopy(G)
     not_inf = 0
